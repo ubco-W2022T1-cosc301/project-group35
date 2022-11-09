9973dabd0a8bc56d138d3937d5c7574de5bda5ce
DakotaBonvie <dakotabonvie@shaw.ca>
2022-11-08 16:41:24 -0800

    done

---
+++ notebooks/project_functions1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+def changedata(url_or_path_to_csv_file):
+
+        dfchange = (
+              pd.read_csv(url_or_path_to_csv_file)    
+              .drop(['No.','Insolation Flux', 'Equilibrium Temperature','Discovery Facility','Discovery Year'], axis=1)
+             .rename(columns={'Num Stars': 'No Of Stars', 'Planet Host': 'Host Star of Planet(s)'}) 
+            .dropna(axis=0)
+            )
+        
+        return dfchange
