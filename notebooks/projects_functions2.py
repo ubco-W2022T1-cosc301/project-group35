f23bdec8d431dd2b3dc9881a0307b647d3555f3f
bolu2710 <bolu2710@gmail.com>
2022-11-28 00:37:46 -0800

    fixed errors

--- notebooks/projects_functions2.py
+++ notebooks/projects_functions2.py
@@ -1,11 +1,10 @@
-import pandas as pd
-import numpy as np
 def change_and_drop(url_or_path_to_csv_file):
 
         df5 = (
               pd.read_csv(url_or_path_to_csv_file)    
-              .drop(['Insolation Flux', 'Equilibrium Temperature'], axis=1)  
+              .drop(['Insolation Flux', 'Equilibrium Temperature', 'Eccentricity','Spectral Type','Gaia Magnitude'], axis=1)  
             .dropna(axis=0)
+            .sort_values(['Discovery Year'], ascending=False)
             )
         
         return df5
